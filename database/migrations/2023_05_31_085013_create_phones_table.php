cf27f9f1e5a76376d317672eb83fc409e78c76c1
LeoPavlovski01 <leopavlovski01@gmail.com>
2023-05-31 11:08:17 +0200

    database Migrations Created

--- database/migrations/2023_05_31_085013_create_phones_table.php
+++ database/migrations/2023_05_31_085013_create_phones_table.php
@@ -11,15 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('phones', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('category_id')->constrained('categories');
+            $table->double('price');
+            $table->longText('image');
+            $table->longText('description')->nullable();
+            $table->string('title');
             $table->timestamps();
             $table->softDeletes();
+            
         });
     }
 
@@ -28,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('phones');
     }
 };
--- database/migrations/2023_05_31_085543_create_orders_table.php
+++ database/migrations/2023_05_31_085543_create_orders_table.php
@@ -11,16 +11,18 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('orders', function (Blueprint $table) {
+            $table->uuid("order_id");
+            $table->decimal("subtotal");
+            $table->decimal("price");
+            $table->decimal("tax");
+            $table->integer("quantity");
+            $table->foreignId("phone_id")->constrained("phones");
+            $table->foreignId("user_id")->constrained("users");
             $table->timestamps();
             $table->softDeletes();
         });
+
     }
 
     /**
@@ -28,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('orders');
     }
 };
